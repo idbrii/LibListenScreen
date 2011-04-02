8bde745e8119af29b0887a048b82be940a2443ed
pydave <pydave@gmail.com>
2011-04-01 17:01:10 -0700

    Use MediaPlayer to play an mp3 when activity starts
    Add dependency on Engoid for ExternalStorage
    
    all hard coded for now, but it starts playback when you open the
    activity and stops it when you leave.

--- src/pydave/audiolib/AudioPlay.java
+++ src/pydave/audiolib/AudioPlay.java
@@ -1,14 +1,61 @@
 
 package pydave.audiolib;
 
+import java.io.File;
+
+import pydave.engoid.sys.ExternalStorage;
 import android.app.Activity;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class AudioPlay extends Activity {
+    static final String TAG = "AudioPlay";
+
+    MediaPlayer player;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.playback);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
+        
+        play();
+    }
+
+    /**
+     * {@inheritDoc}
+     * @see Activity#onStop()
+     */
+    protected void onStop()
+    {
+        if (player != null && player.isPlaying()) {
+            player.stop();
+        }
+
+        super.onStop();
+    }
+
+    public void play() {
+        final ExternalStorage ext = new ExternalStorage();
+
+        // I've placed some test mp3s in:
+        // /sdcard/Android/data/pydave.demo/files/
+        final File f = ext.getFile("pydave.demo",
+        // "music.mp3");
+        // "podcast.mp3");
+              "shortpod.mp3");
+
+        if (f == null) {
+            // TODO: error message
+            return;
+        }
+
+        final Uri uri = Uri.fromFile(f);
+        player = MediaPlayer.create(this, uri);
+        player.start();
     }
 }
